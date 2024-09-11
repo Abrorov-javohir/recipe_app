130c4c1863d7a487e7c4613725856e85bac0e450
Hunter20050910 <145260700+Abrorov-javohir@users.noreply.github.com>
2024-09-11 20:51:45 +0500

    ttt

--- lib/topshiriq.dart
+++ lib/topshiriq.dart
@@ -8,10 +8,20 @@
 //   print(mix);
 // }
 
-
 // void main() {
 //   String mix = '';
 //   List<String> a = ["Banan", "Olma", "Anor"];
 //   a.forEach(print);
 //   print(mix);
 // }
+
+void main() {
+  String mix = '';
+  List<String> a = ["Banan", "Olma", "Anor"];
+
+  for (int i = 0; i < a.length; i++) {
+    mix += i.toString();
+  }
+
+  print(mix);
+}
